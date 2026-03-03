415766a7493e393d4f2ca88f6daedfe15f17585e
Nicolas Pastorini <83242411+NicoPastorini1@users.noreply.github.com>
2026-03-03 12:03:31 -0300

    chore: remove build and dependency folders from repo

--- .tmp/build/src/utils/endMarkerShapes.d.ts
+++
@@ -1,2 +0,0 @@
-import * as d3 from "d3";
-export declare function renderEndMarkerShape(selection: d3.Selection<SVGGElement, any, any, any>, shapeType: number, x: number, y: number, size: number, fill: string, stroke: string, strokeWidth: number): void;
